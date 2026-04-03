7a7066ad7dcb9f227afb06e747e9de96c833a77c
Abhijit Panickar <abhijitdeepak2003@gmail.com>
2026-04-03 21:13:00 +0530

    fix: api base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000/api'
+  baseURL: process.env.REACT_APP_API_URL || "https://code-collab-backend-427n.onrender.com"
 });
 
 // ✅ TOKEN FIX
